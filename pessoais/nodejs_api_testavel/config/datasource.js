356873f844090a2a5280b72e9b2bc3b712e06e15
Lucas Tavares <lucastavaresdarocha@gmail.com>
2017-06-12 21:48:22 -0300

    SQLite3, Sequelize e init models

---
+++ pessoais/nodejs_api_testavel/config/datasource.js
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize';
+import fs from 'fs';
+import path from 'path';
+
+let database = null;
+
+export default (app) => {
+    if (!database) {
+        const config = app.config;
+        const sequelize = new Sequelize(
+                config.database,
+                config.username,
+                config.password,
+                config.params
+            );
+
+        database = {
+            sequelize,
+            Sequelize,
+            models: {}
+        };
+
+        sequelize.sync().done() => {
+            return database;
+        }
+    }
+
+    return database;
+};
